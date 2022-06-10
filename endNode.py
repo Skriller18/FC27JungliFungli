913039c3b4902fe3ed745dbebf6a282d0525cebf
rahulanilal <105976063+rahulanilal@users.noreply.github.com>
2022-06-11 00:50:58 +0530

    Update endNode.py

--- endNode.py
+++ endNode.py
@@ -1,7 +1,24 @@
 import json
 import re
 
-endInput = 'IEM'
+endInput = 'MCA020'
+
+
+with open('/Users/rahulanilal/Javascript/ClassToDep.json', 'r') as f:
+    optimizeDestination = json.load(f)
+
+def tagger(str1):
+    flag = False
+    for i in range(len(optimizeDestination)):
+        if optimizeDestination[i]["tag"] in str1:
+            flag = True
+            break
+    if flag == True:
+        endInput = optimizeDestination[i]["department"]
+        print(endInput)
+
+tagger(endInput)
+
 
 def getEndNode():
     with open('/Users/rahulanilal/Javascript/CampusCoordinates.json', 'r') as f:
